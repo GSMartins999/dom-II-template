0b2c06cff56270b82885161e039e1d9519800c37
Giovanni Souza <GSMartins999>
2023-08-06 16:32:25 -0300

    dom-2-template-feito

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,8 +1,13 @@
 const insereItem = () => {
+    ///criei o elemento dentro da lista
     const listaAtual = document.getElementById("lista")
     const novoConteudo = document.createElement("li")
+    ///aqui disse que o elemento que seria jogado dentro da lista seria o do meu-input
     const digitarConteudo = document.getElementById("meu-input")
+    ///Aqui disse que o elemento(novo conteudo) criado seria o filho da lista
     listaAtual.appendChild(novoConteudo)
-
+    //Aqui joguei o elemento criado para o fim da lista
     novoConteudo.innerHTML = digitarConteudo.value
+    ///Aqui atribui um valor 0 para o input para que ao enviar o elemento, não fique o nome na barra do input
+    digitarConteudo.value = ""
 }
